4bb0ae2dd255f60015840c481e17ba7bed6f9b86
github username <github email address>
2020-07-04 19:26:50 +0100

    Server running

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'))
+
+/* When deployed the PORT will come from the environment variable. 
+Locally we can use a port of our choice */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
