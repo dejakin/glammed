0b48df7eb249f8b3aede6dccf1fd5270b53d5800
github username <github email address>
2020-07-08 18:27:28 +0100

    Express Router set up for base endpoints (users, profile, auth and posts

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'))
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 /* When deployed the PORT will come from the environment variable. 
 Locally we can use a port of our choice */
