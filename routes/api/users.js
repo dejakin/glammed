b195fbd57d15f4290c7372437bb90a97b5052939
github username <github email address>
2020-07-16 20:04:53 +0100

    Implemented functionality to send a JSON Web Token back to the client upon user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -47,7 +49,19 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        // If error exists, control will be passed to catch block along with the thrown error object
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
 
     } catch (err) {
         console.error(err.message);
