b8906a586814679a620f74fc612f45033c102358
github username <github email address>
2020-07-29 22:57:09 +0100

    Create POST route for storing user post in DB

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => res.send('Post route'));
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// POST to api/posts. Creates post in DB
+router.post('/', [ auth, [
+    check('text', 'Text is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            username: user.username,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
