2252cd486343aea719f257bb1d7871d1b06ac057
github username <github email address>
2020-07-31 22:01:27 +0100

    Create route for fetching all posts from DB
    Posts are fetched from the DB in the order of most recent first

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,15 @@ router.post('/', [ auth, [
     }
 });
 
+// GET to /api/posts
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}) 
 module.exports = router;
