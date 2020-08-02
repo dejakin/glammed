43b907ba192b2ab03182ae6018e6274084c9f3f8
github username <github email address>
2020-08-02 11:16:19 +0100

    Create route to fetch all posts for a specific user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,8 +35,7 @@ router.post('/', [ auth, [
     }
 });
 
-// GET to /api/posts
-
+// GET to /api/posts. Returns all posts in DB
 router.get('/', auth, async (req, res) => {
     try {
         const posts = await Post.find().sort({ date: -1 });
@@ -45,5 +44,27 @@ router.get('/', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-}) 
+}); 
+
+// GET to api/posts/:username. Finds all posts for a specific user
+router.get('/:username', auth, async (req, res) => {
+    try {
+        const posts = await Post.find({ username: req.params.username }).sort({ date: -1 });
+        const userExists = await User.findOne({ username: req.params.username });
+
+        if(!userExists) {
+           return res.status(400).json({ msg: 'Profile not found' });
+        }
+
+        if(posts.length == 0) {
+           return res.status(400).json({ msg: 'This user has no posts' });
+        }
+
+        res.json(posts);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}); 
+
 module.exports = router;
