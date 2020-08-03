1cb96e098838522016d78e9f33f4cb887a11ee20
github username <github email address>
2020-08-03 13:46:26 +0100

    Create route to unlike a post
    Server maps 'likes' array to find the index of the like which needs to be removed. Like 120 checks to see ifthe like exists before proceeding

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -110,4 +110,27 @@ router.put('/like/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// PUT to /api/posts/unlike/:id. Adds a like to the specified post
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if post has already been liked by user
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: "Post has not yet been liked" });
+        }
+
+        // Get element index to be removed
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+        await post.save();
+
+        res.json(post.likes)
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
