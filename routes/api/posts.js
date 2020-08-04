094fa78c778b937877f5163edeef911ee6a5fb02
github username <github email address>
2020-08-04 17:18:34 +0100

    Create route to add comment to specified post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -111,7 +111,7 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 });
 
-// PUT to /api/posts/unlike/:id. Adds a like to the specified post
+// PUT to /api/posts/unlike/:id. Removes a like from the specified post
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
@@ -127,6 +127,37 @@ router.put('/unlike/:id', auth, async (req, res) => {
         await post.save();
 
         res.json(post.likes)
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// POST to api/posts/comment/:id. Adds a comment under a post
+router.post('/comment/:id', [ auth, [
+    check('text', 'Text is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            username: user.username,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+        res.json(post.comments);
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server Error');
