c73b5932a8aa989d5b472799564c6949b4f02bfe
github username <github email address>
2020-08-10 23:29:23 +0100

    Create component for welcome page body

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import Welcome from './components/layout/Welcome';
 import './App.css';
 
 const App = () => 
     <Fragment>
       <Navbar />
+      <Welcome />
     </Fragment>
 
 
---
+++ client/src/components/layout/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Welcome = () => {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>G L A M M E D</h1>
+                    <p>
+                        Create a beautician profile - share your work and get noticed by potential customers
+                    </p>
+                    <div>
+                        <a href="#">Sign Up</a>
+                        <a href="#">Login</a>
+                    </div>
+                </div>
+            </div>
+        </section> 
+    )
+}
+
+export default Welcome;
