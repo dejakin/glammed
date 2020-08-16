1725d2ff2dc56389c65b53f7e85da0108738d756
github username <github email address>
2020-08-16 14:22:29 +0100

    Create user registration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large">REGISTER</h1>
+            <p><i className="fas fa-user"></i>Create Your Account</p>
+            <form className="form" action="create-profile.html">
+                <div className="form-section">
+                    <input type="text" placeholder="First Name" name="Firstname" required />
+                </div>
+                <div className="form-section">
+                    <input type="text" placeholder="Last Name" name="Lastname" required />
+                </div>
+                <div className="form-section">
+                    <input type="email" placeholder="Email Address" name="email" />
+                </div>
+                <div className="form-section">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="8"
+                    />
+                </div>
+                <div className="form-section">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="8"
+                    />
+                </div>
+                <input type="submit" className="btn1" value="Register" />
+            </form>
+            <p className="note">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
 
