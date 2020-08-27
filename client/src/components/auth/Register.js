52eb9a1d77914ddc54cf1f3637502ae95a313443
github username <github email address>
2020-08-27 23:49:36 +0100

    Dispatch SET_ALERT action if passwords do not match
    If client enters passwords which do not match, the SET_ALERT action will be dispatched and the alert reducer will update the store with the data in the payload. Our alert component will use the data in the payload to display a message on the UI

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
 
 
-const Register = () => {
+const Register = (props) => {
 
     const [formData, setFormData] = useState({
         forename: '',
@@ -19,7 +21,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            console.log('Passwords do not match');
+            props.setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
         }
@@ -91,4 +93,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default connect(null, { setAlert })(Register);
