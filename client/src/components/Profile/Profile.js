19b544a48984da2b46e007b6ba858ad92cd84f7b
github username <github email address>
2020-09-11 15:04:32 +0100

    Render current user's name on profile page in welcome message
    The current user's name is dynamically outputted depending on who is logged in. If getCurrentProfile has not returned, the spinner will show. If it is returned, the 'profile' and 'loading' properties of the profile slice of the store will be updated and therefore the welcome message with the logged in user's username will render

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -3,12 +3,17 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Profile = ({ getCurrentProfile, auth, profile: { profile, loading } }) => {
+const Profile = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    return loading && profile === null ? <Spinner /> : <Fragment>test</Fragment>;
+    return loading && profile === null ? <Spinner /> : <Fragment>
+        <h1 className="large">PROFILE</h1>
+        <p>
+            <i className='fas fa-user'></i> Welcome <b>{ user && user.username }</b>
+        </p>
+    </Fragment>;
 }
 
 const mapStateToProps = state => ({
