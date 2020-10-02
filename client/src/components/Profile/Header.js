72fffd629b69d5af785f4ae245c0814c634702f7
github username <github email address>
2020-10-02 15:55:51 +0100

    Center header component horizontally

--- client/src/components/Profile/Header.js
+++ client/src/components/Profile/Header.js
@@ -8,30 +8,30 @@ const Header = ({ profile: {
     social
 } }) => {
     return (
-        <div class="profile-top bg-primary p-2">
+        <div className="profile-header bg-primary p-2">
             <img
-                class="round-img my-1"
+                className="round-img my-1"
                 src={user.avatar}
                 alt=""/>
-            <h1 class="large">{username}</h1>
-            <p class="lead">{bio}</p>
+            <h1>{username}</h1>
+            <p>{bio}</p>
             <p>{location}</p>
-            <div class="icons my-1">
+            <div className="icons my-1">
                 {social.twitter && 
                 <a href={social.twitter} target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-twitter fa-2x"></i>
+                <i className="fab fa-twitter fa-2x"></i>
                 </a>}
                 {social.facebook &&                 
                 <a href={social.facebook} target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-facebook fa-2x"></i>
+                <i className="fab fa-facebook fa-2x"></i>
                 </a>}
                 {social.youtube && 
                 <a href={social.youtube} target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-youtube fa-2x"></i>
+                <i className="fab fa-youtube fa-2x"></i>
                 </a>}
                 {social.instagram && 
                 <a href={social.instagram} target="_blank" rel="noopener noreferrer">
-                <i class="fab fa-instagram fa-2x"></i>
+                <i className="fab fa-instagram fa-2x"></i>
                 </a>}
             </div>
         </div>
